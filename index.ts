7355d225421663835fdf26148a185a987c2fc7a6
Sávio Muniz <savio.muniz@vtex.com.br>
2020-07-26 21:43:11 -0300

    Project setup - graphql, express, prettier, typescript, eslint

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { buildSchema } from 'graphql'
+// import { ApolloServer, graphiqlExpress } from 'apollo-server-express'
+import fs from 'fs'
+import resolvers from './src/graphql/resolvers'
+
+const PORT = process.env.PORT || 8080
+const SCHEMA_PATH = './src/graphql/schema.graphql'
+const schema = () => buildSchema(fs.readFileSync(SCHEMA_PATH).toString())
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema(),
+  rootValue: resolvers,
+  graphiql: true,
+}))
+
+app.listen(PORT, () => console.log(`Code stocks running on ${PORT}`))
